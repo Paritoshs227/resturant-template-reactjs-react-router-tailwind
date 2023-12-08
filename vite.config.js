0bd54f924fa4e64bf87ea2cc2a83a1e50d21bc9c
paritosh <paritoshs227@gmail.com>
2023-12-08 19:43:58 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/resturant-template-reactjs-react-router-tailwind/"
 })
